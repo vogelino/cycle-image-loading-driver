6d7fe057c1523cb4285a7d1e93af0d99c0e45b1b
vogelino <contact@vogelino.com>
2020-06-18 13:30:08 -0400

    Emit event at loading start as well

--- src/index.js
+++ src/index.js
@@ -17,14 +17,16 @@ export const makeImageLoadingDriver = () => {
 		from(sink$).subscribe({
 			next(images) {
 				const imagesPromises = images.map((src) => {
-					const imgSuccess = { src, loaded: true }
+					const imgSuccess = { src, loaded: true, loading: false }
 					if (!!imageCache[src]) return Promise.resolve(imgSuccess)
+					imageCache[src] = { src, loaded: false, loading: true }
+					source$.next(imageCache)
 					return loadImage(src)
 						.then(() => {
 							imageCache[src] = imgSuccess
 						})
 						.catch((error) => {
-							imageCache[src] = { src, loaded: false, error }
+							imageCache[src] = { src, loaded: false, loading: false, error }
 						})
 				})
 
